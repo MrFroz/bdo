30d5928f83d570429d1994e5d3a04505acab6b95
MrFroz <theoma91@icloud.com>
2019-09-20 16:15:10 +0200

    Add files via upload

--- login.js
+++ login.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
   $("loginform").hide()
   $("pill").click(function(){
+    var act0 = true;
+    if(act0){
     $("img").animate({top: '50%'});
     $('img').addClass('a');
     $("t").fadeOut(100);
     $("header").animate({height: '60px'});
     $("loginform").fadeIn(3000);
+    act0 = false;
+  }
+  });
+  $("a").click(function(){
+  $("a").addClass("is-loading");
+  setTimeout(function(){
+    $("loginform").animate({left: '5000px'});
+    $("loginform").fadeOut();
+    setTimeout(function(){
+    $("header").animate({height: '100%',width: '100px'});
+    $("img").removeClass("a");
+    $("img").animate({top: '40px'});
+  }, 200);
+}, 4000);
   });
 });
